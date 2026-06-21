cc26c250e88dc7a70008cf075ccfb107c5fdab4e
Daniel <taisublack@gmail.com>
2026-06-21 18:15:17 +0100

    chore: scaffold segmented control package

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.js",
+      formats: ["es"],
+      fileName: "index",
+    },
+  },
+  test: {
+    environment: "jsdom",
+  },
+});
